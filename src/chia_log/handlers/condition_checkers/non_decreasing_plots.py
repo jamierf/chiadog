f18801734422a87d6fd668aaecafc0a7311ff172
andymarden <63465082+andymarden@users.noreply.github.com>
2021-05-31 07:34:26 +0100

    Update non_decreasing_plots.py
    Added "time since last plot" to the notification of a new plot

--- src/chia_log/handlers/condition_checkers/non_decreasing_plots.py
+++ src/chia_log/handlers/condition_checkers/non_decreasing_plots.py
@@ -17,15 +17,22 @@ class NonDecreasingPlots(HarvesterConditionChecker):
     def __init__(self):
         logging.info("Enabled check for non-decreasing total plot count.")
         self._max_farmed_plots = 0
+        self._last_plot_time = datetime.datetime.now()
 
     def check(self, obj: HarvesterActivityMessage) -> Optional[Event]:
         event = None
         if obj.total_plots_count > self._max_farmed_plots:
             logging.info(f"Detected new plots. Farming with {obj.total_plots_count} plots.")
             self._max_farmed_plots = obj.total_plots_count
+            
+            # Work out time since last plot (or startup)
+            prev_plot_time = self._last_plot_time
+            self._last_plot_time = datetime.datetime.now()
+            time_since_prev_plot = str(self._last_plot_time - prev_plot_time).split(".")[0]
+
             #Notification of change in plot count fires with increase in plots and on initial startup 
             message = (
-                f"Detected new plots. Farming with {obj.total_plots_count} plots."
+                f"Detected new plot after {time_since_prev_plot} - farming with {obj.total_plots_count} plots."
             )           
             event = Event(
                 type=EventType.USER, priority=EventPriority.LOW, service=EventService.HARVESTER, message=message
